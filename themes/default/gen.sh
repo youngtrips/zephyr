033bf02050296cfbb941ce0bf5df92e6cfce2bef
Tuz <youngtrips@gmail.com>
2012-06-01 11:51:41 +0800

    update

---
+++ themes/default/gen.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python2 test.py > a.html
--- themes/default/test.py
+++ themes/default/test.py
@@ -10,7 +10,50 @@
 from mako.template import Template
 from mako.lookup import TemplateLookup
 
+
+class Foo:
+    pass
+
+
+site = Foo()
+post = Foo()
+page = Foo()
+
+
+site.name = "MindEden"
+site.author = "Tuz"
+site.url = "http://mindeden.com"
+site.pagelimit = 10
+site.pages = []
+site.categories = []
+site.posts = []
+
+
+page.title = "Home"
+page.name = "Home"
+page.url = site.url
+
+#site.pages.append(page)
+
+cate = Foo()
+cate.name = "default"
+cate.url = site.url + "/categories/" + cate.name
+
+site.categories.append(cate)
+
+post.title = "Welcome to MindEden"
+post.date = "2012-06-01"
+post.time = "10:55:02"
+post.cate = cate
+post.url = site.url + "/post/2012/06/01/welcome-to-mindeden/"
+post.content = "Welcome to MindEden, it's coming soon!!!"
+
+
+page.title = post.title
+
+site.posts.append(post)
+
 mylookup = TemplateLookup(directories=['.'])
-tpl = mylookup.get_template("page.html")
-print tpl.render()
+tpl = mylookup.get_template("post.html")
+print tpl.render(site=site, post=post, page=page)
 
