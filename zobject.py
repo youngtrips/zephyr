3acab8b218637c174c9f110fd0813f8f045f09b3
youngtrips <youngtrips@mindeden.(none)>
2012-06-03 22:12:27 +0800

    update

---
+++ zobject.py
@@ -0,0 +1,30 @@
+#-*- coding: utf-8 -*-
+
+# Author: youngtrips(youngtrips@gmail.com)
+# Created Time:  2012-06-03
+# File Name: zobject.py
+# Description: 
+#
+
+class Post(object):
+    def __init__(self):
+        pass
+
+
+class Category(object):
+    def __init__(self):
+        pass
+
+
+class Tag(object):
+    def __init__(self):
+        pass
+
+class Site(object):
+    def __init__(self):
+        pass
+
+class Node(object):
+    def __init__(self):
+        pass
+
