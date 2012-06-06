7ab5d6790cf10b27385cbaa51dd3f235b6191f6b
Tuz <youngtrips@gmail.com>
2012-06-06 18:02:22 +0800

    update

---
+++ src/template/config.py
@@ -0,0 +1,33 @@
+site = {
+    'name': 'MindEden',
+    'description': 'Somewhere to keep my iears',
+    'url': 'http://mindeden.com',
+    'pagelimit': 10,
+    'theme': 'default',
+    'disqus_shortname': 'mindeden',
+}
+
+author = {
+    'name': 'Tuz',
+    'email': 'youngtrips@gmail.com',
+}
+
+repo = {
+#name: url
+'github': 'git@github.com:youngtrips/mindeden.git',
+}
+
+rsync = {
+#name: url
+'myvps': 'root@codedelight:/var/www'
+}
+
+links = {
+#title: url
+'kevinlynx': 'http://codemacro.com',
+'Fedoraproject': 'http://fedoraproject.org',
+}
+
+comment = {
+}
+
