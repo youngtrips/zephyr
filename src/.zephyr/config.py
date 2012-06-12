91d7267092ddd70e6dc658155b123deac05505f4
Tuz <youngtrips@gmail.com>
2012-06-12 17:36:21 +0800

    update

---
+++ src/.zephyr/config.py
@@ -0,0 +1,35 @@
+# configuration
+# site configuration
+SITE = {
+    'name': 'MindEden',
+    'description': 'Somewhere to keep my iears',
+    'url': 'http://mindeden.com',
+    'pagelimit': 10,
+    'theme': 'default',
+    'disqus_shortname': 'mindeden',
+}
+
+AUTHOR = {
+    'name': 'Tuz',
+    'email': 'youngtrips@gmail.com',
+}
+
+# remote git repo list
+REPO = {
+    #name: url
+    'github': 'git@github.com:youngtrips/mindeden.git',
+}
+
+# remote rsync server list
+RSYNC = {
+    #name: url
+    'myvps': 'root@codedelight:/var/www'
+}
+
+# friend links linux
+LINKS = {
+    #title: url
+    'kevinlynx': 'http://codemacro.com',
+    'Fedoraproject': 'http://fedoraproject.org',
+}
+
